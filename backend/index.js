08018a96c39c78e67aab7e174b444591e1220b13
tushar-goyal-25 <pottertushar@gmail.com>
2024-07-23 19:46:25 +0530

    setup test for get request endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 const cors = require('cors');
 const app = express()
 
-
+app.use('/' , require('./routes/authroutes'))
 
 const port = 8000;
 
---
+++ backend/routes/authroutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router()
+const cors = require('cors')
+const {test} = require('../controlers/authcontroler')
+
+//middleware
+
+//connecting backedn with frontend
+router.use(
+    cors({
+        credentials:true ,
+        origin:'http://localhost:5173'
+
+}))
+
+
+router.get('/', test)
+
+module.exports = router
