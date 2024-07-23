73ea71f67564bd643d3a68da89428a5be1e0bf5e
tushar-goyal-25 <pottertushar@gmail.com>
2024-07-23 19:15:30 +0530

    added login and register forms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default function Login(){
+    const loginUser = (e) => {
+        e.preventDefault()
+    }
     return(
-        <div>Login page</div>
+        <div>
+            <form onSubmit={loginUser}>
+            <label>Email</label>
+                <input type="email" placeholder="Enter Email" />
+                <label>Password</label>
+                <input type="password" placeholder="Enter Password" />
+                <button type="submit">Login</button>
+
+            </form>
+        </div>
     )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 export default function Register(){
+
+    
+    const registerUser  = (e) => {
+        e.preventDefault()
+    }
     return(
-        <div>register page</div>
+
+        <div>
+            <form onSubmit={registerUser}>
+                <label>Name</label>
+                <input type="text" placeholder="Enter Name" />
+                <label>Email</label>
+                <input type="email" placeholder="Enter Email" />
+                <label>Password</label>
+                <input type="password" placeholder="Enter Password" />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
